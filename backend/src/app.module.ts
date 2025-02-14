cf6f866d7b81c7a91cede9e0c69549eb6cc0e6cb
RareSecond <jasper@codictive.be>
2025-02-14 22:10:18 +0100

    Removed app controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
@@ -13,7 +12,6 @@ import { DatabaseModule } from './database/database.module';
     UserModule,
     DatabaseModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
