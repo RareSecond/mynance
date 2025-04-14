95d99aeea1ac2ee97eca1dc4015fde3e751aa811
RareSecond <jasper@codictive.be>
2025-04-14 10:03:21 +0200

    Added purple theme

--- frontend/src/routes/~_nonAuthRoutes/~login.tsx
+++ frontend/src/routes/~_nonAuthRoutes/~login.tsx
@@ -15,19 +15,21 @@ function RouteComponent() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <img src={logo} alt="logo" className="w-80 mb-4" />
-      <button
-        onClick={handleGoogleLogin}
-        className="px-4 py-2 border flex gap-2 cursor-pointer border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
-      >
-        <img
-          className="w-6 h-6"
-          src="https://www.svgrepo.com/show/475656/google-color.svg"
-          loading="lazy"
-          alt="google logo"
-        />
-        <span>Login with Google</span>
-      </button>
+      <div className="flex flex-col items-center justify-center bg-dark-card rounded-lg px-8 py-20">
+        <img src={logo} alt="logo" className="w-80 mb-4" />
+        <button
+          onClick={handleGoogleLogin}
+          className="px-4 py-2 border flex gap-2 cursor-pointer border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
+        >
+          <img
+            className="w-6 h-6"
+            src="https://www.svgrepo.com/show/475656/google-color.svg"
+            loading="lazy"
+            alt="google logo"
+          />
+          <span>Login with Google</span>
+        </button>
+      </div>
     </div>
   );
 }
