a303afb790c01adb72e78cd34ede6a1a58b67e6a
RareSecond <jasper@codictive.be>
2025-04-28 22:15:48 +0200

    Extended CORS to allow codictive.be

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [/^http:\/\/localhost:\d+$/],
+    origin: [/^http:\/\/localhost:\d+$/, /^https?:\/\/codictive\.be$/],
     credentials: true,
   });
   app.use(cookieParser());
