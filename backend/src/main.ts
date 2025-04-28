04bf49c453ed68731801bfe1166d0ef665c65339
RareSecond <jasper@codictive.be>
2025-04-28 22:23:17 +0200

    Also allowed subdomains

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [/^http:\/\/localhost:\d+$/, /^https?:\/\/codictive\.be$/],
+    origin: [
+      /^http:\/\/localhost:\d+$/,
+      /^https?:\/\/(?:[\w-]+\.)*codictive\.be$/,
+    ],
     credentials: true,
   });
   app.use(cookieParser());
