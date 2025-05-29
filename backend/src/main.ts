6a61711766595fd66940a43f059b66b6fa6ae97f
RareSecond <jasper@codictive.be>
2025-05-29 11:21:48 +0200

    Set up swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mynance API')
+    .setDescription('The Mynance API description')
+    .setVersion('0.0.1')
+    .addTag('mynance')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.enableCors({
     origin: [
       /^http:\/\/localhost:\d+$/,
