ac6ed257f9898f660841e2528d5dddd397a1a213
RareSecond <jasper@codictive.be>
2025-05-30 16:37:34 +0200

    Added the ability to generate a swagger.json file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,18 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Mynance API')
-    .setDescription('The Mynance API description')
-    .setVersion('0.0.1')
-    .addTag('mynance')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Mynance API')
+      .setDescription('The Mynance API description')
+      .setVersion('0.0.1')
+      .addTag('mynance')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    const jsonString = JSON.stringify(document, null, 2);
+    fs.writeFileSync('./swagger.json', jsonString);
+
+    SwaggerModule.setup('api', app, document);
+  }
 
   app.enableCors({
     origin: [
