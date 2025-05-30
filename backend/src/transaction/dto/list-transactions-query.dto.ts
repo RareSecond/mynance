1bda38f80e62e6855dd3e988250d72f29ff64fec
RareSecond <jasper@codictive.be>
2025-05-30 22:04:28 +0200

    Improved transactions DTO

--- backend/src/transaction/dto/list-transactions-query.dto.ts
+++ backend/src/transaction/dto/list-transactions-query.dto.ts
@@ -2,8 +2,8 @@ import { Transform } from 'class-transformer';
 import { IsOptional, IsBoolean } from 'class-validator';
 
 export class ListTransactionsQueryDto {
-  @IsOptional()
   @IsBoolean()
+  @IsOptional()
   @Transform(({ value }) => value === 'true')
-  uncategorizedOnly?: boolean;
+  uncategorizedOnly?: boolean = false;
 }
--- backend/src/transaction/transaction.controller.ts
+++ backend/src/transaction/transaction.controller.ts
@@ -1,26 +1,29 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
-import { ImportTransactionsDto } from './dto/import-transactions.dto';
-import { ListTransactionsQueryDto } from './dto/list-transactions-query.dto';
 import { TransactionResponseDto } from './dto/transaction-response.dto';
 import { LinkCategoryDto } from './dto/link-category.dto';
+import { ListTransactionsQueryDto } from './dto/list-transactions-query.dto';
+import { ImportTransactionsDto } from './dto/import-transactions.dto';
 import {
   GetAnalyticsQueryDto,
   AnalyticsCategoryDto,
 } from './dto/get-analytics.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('transaction')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
   @Get()
+  @ApiOperation({ summary: 'List transactions' })
   async listTransactions(
-    @Query('uncategorizedOnly') uncategorizedOnly = false,
+    @Query() query: ListTransactionsQueryDto,
   ): Promise<TransactionResponseDto[]> {
-    return this.transactionService.listTransactions(uncategorizedOnly);
+    return this.transactionService.listTransactions(query.uncategorizedOnly);
   }
 
   @Get('analytics')
+  @ApiOperation({ summary: 'Get transaction analytics' })
   async getAnalytics(
     @Query() query: GetAnalyticsQueryDto,
   ): Promise<AnalyticsCategoryDto[]> {
@@ -31,11 +34,13 @@ export class TransactionController {
   }
 
   @Post('import')
+  @ApiOperation({ summary: 'Import transactions' })
   async importTransactions(@Body() importDto: ImportTransactionsDto) {
     return this.transactionService.importTransactions(importDto.accountId);
   }
 
   @Post(':transactionId/category')
+  @ApiOperation({ summary: 'Link a category to a transaction' })
   async linkCategory(
     @Param('transactionId') transactionId: string,
     @Body() linkCategoryDto: LinkCategoryDto,
