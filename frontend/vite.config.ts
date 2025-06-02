31a519a8874ea9e7f2a8bc2f45971ba7ade1443d
RareSecond <jasper@codictive.be>
2025-06-02 11:10:21 +0200

    Better setup FE typechecking

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,9 +9,6 @@ export default defineConfig({
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     tailwindcss(),
-    checker({
-      typescript: true,
-    }),
   ],
   resolve: {
     alias: {
