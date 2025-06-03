68d9695560f09496729c1b1a6b3c7174ad05ee0f
RareSecond <jasper@codictive.be>
2025-06-03 13:38:28 +0200

    Fixed TS errors

--- frontend/src/routes/~__root.tsx
+++ frontend/src/routes/~__root.tsx
@@ -1,25 +1,9 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import React, { Suspense } from "react";
-
-const TanStackRouterDevtools =
-  process.env.NODE_ENV === "production"
-    ? () => null // Render nothing in production
-    : React.lazy(() =>
-        // Lazy load in development
-        import("@tanstack/router-devtools").then((res) => ({
-          default: res.TanStackRouterDevtools,
-          // For Embedded Mode
-          // default: res.TanStackRouterDevtoolsPanel
-        }))
-      );
 
 export const Route = createRootRoute({
   component: () => (
     <div className="pt-8 px-4">
       <Outlet />
-      {/* <Suspense>
-        <TanStackRouterDevtools />
-      </Suspense> */}
     </div>
   ),
 });
